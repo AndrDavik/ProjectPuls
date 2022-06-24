3f6f0510a462d25549a529c9d5ade4adfe56ea1e
Andrey <davikandrey@gmail.com>
2022-06-24 12:56:21 +0300

    add adaptive for slider 992px

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img scr="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img scr="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-      });
+	$('.carousel__inner').slick({
+		speed: 1200,
+		// adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img scr="icons/left.svg" alt="prev"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img scr="icons/right.svg" alt="next"></button>',
+		dotsClass: 'slick-dots',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	  });
   });
