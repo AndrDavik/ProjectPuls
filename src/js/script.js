c17ad6e288d92a67f950a838ec02c18d338a2f7e
Andrey <davikandrey@gmail.com>
2022-06-28 18:06:35 +0300

    add mailer

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,26 @@ $(document).ready(function(){
 	valideForm('#consultation-form');
 	valideForm('#consultation form');
 	valideForm('#order form');
+
+	$('input[name=phone]').mask("+8 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #tnx').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
   });
