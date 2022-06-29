344dc335b5969854c67ca0d6389c93e7f09ac7d9
Andrey <davikandrey@gmail.com>
2022-06-29 17:47:50 +0300

    mod media.css

--- src/js/script.js
+++ src/js/script.js
@@ -108,11 +108,13 @@ $(document).ready(function(){
 		}
 	});
 
-	$("a[href^='#']").click(function() {
+	$("a[href=#up], [href=#ctlg]").click(function() {
 		const _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
 	});
+
+	new WOW().init();
 
 
   });
